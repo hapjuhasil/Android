e017b7e3c69f6362e5d7dc586ad5e2e63af6bf64
김명석 <twinaudtjr@naver.com>
2024-08-01 23:42:18 +0900

    [feat] CustomDividerItemDecoration

---
+++ happle/app/src/main/java/com/example/happle/CustomDividerItemDecoration.kt
@@ -0,0 +1,27 @@
+package com.example.happle
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.drawable.Drawable
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+
+class CustomDividerItemDecoration(context: Context) : RecyclerView.ItemDecoration() {
+
+    private val divider: Drawable? = ContextCompat.getDrawable(context, R.drawable.divider)
+
+    override fun onDrawOver(c: Canvas, parent: RecyclerView, state: RecyclerView.State) {
+        val left = parent.paddingLeft
+        val right = parent.width - parent.paddingRight
+
+        val childCount = parent.childCount
+        for (i in 0 until childCount) {
+            val child = parent.getChildAt(i)
+            val params = child.layoutParams as RecyclerView.LayoutParams
+            val top = child.bottom + params.bottomMargin
+            val bottom = top + (divider?.intrinsicHeight ?: 0)
+            divider?.setBounds(left, top, right, bottom)
+            divider?.draw(c)
+        }
+    }
+}
